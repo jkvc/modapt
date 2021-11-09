30612df8b8fe57893c069cecd554a0969150c437
Junshen Kevin Chen <kevinehc@gmail.com>
2021-11-08 22:40:30 -0800

    __main__ guard run_demo ; gp

--- modapt/demo/run_demo.py
+++ modapt/demo/run_demo.py
@@ -7,41 +7,42 @@ from modapt.modapt import (
     roberta_train,
 )
 
-_THIS_DIR = dirname(realpath(__file__))
+if __name__ == "__main__":
+    _THIS_DIR = dirname(realpath(__file__))
 
-print(">> train logreg model")
-model, vocab, metrics = logreg_train(
-    data=join(_THIS_DIR, "demo_data", "train.csv"),
-    save_model_dir=join(_THIS_DIR, "models", "logreg"),
-    vocab_size=100,
-)
-print(">> train logreg model done")
+    print(">> train logreg model")
+    model, vocab, metrics = logreg_train(
+        data=join(_THIS_DIR, "demo_data", "train.csv"),
+        save_model_dir=join(_THIS_DIR, "models", "logreg"),
+        vocab_size=100,
+    )
+    print(">> train logreg model done")
 
-print(">> predict eval logreg model")
-scores, est_acc = logreg_predict_eval(
-    data_labeled=join(_THIS_DIR, "demo_data", "valid_labeled.csv"),
-    data_unlabeled=join(_THIS_DIR, "demo_data", "valid_unlabeled.csv"),
-    model_dir=join(_THIS_DIR, "models", "logreg"),
-)
-print(">> predict eval logreg model done")
-print(">> estimated acc:", est_acc)
-print(">> scores shape:", scores.shape)
+    print(">> predict eval logreg model")
+    scores, est_acc = logreg_predict_eval(
+        data_labeled=join(_THIS_DIR, "demo_data", "valid_labeled.csv"),
+        data_unlabeled=join(_THIS_DIR, "demo_data", "valid_unlabeled.csv"),
+        model_dir=join(_THIS_DIR, "models", "logreg"),
+    )
+    print(">> predict eval logreg model done")
+    print(">> estimated acc:", est_acc)
+    print(">> scores shape:", scores.shape)
 
-print(">> train roberta model")
-model, metrics = roberta_train(
-    train_data=join(_THIS_DIR, "demo_data", "train.csv"),
-    valid_data=join(_THIS_DIR, "demo_data", "valid_labeled.csv"),
-    save_model_dir=join(_THIS_DIR, "models", "roberta"),
-    max_epochs=4,
-)
-print(">> train roberta model done")
+    print(">> train roberta model")
+    model, metrics = roberta_train(
+        train_data=join(_THIS_DIR, "demo_data", "train.csv"),
+        valid_data=join(_THIS_DIR, "demo_data", "valid_labeled.csv"),
+        save_model_dir=join(_THIS_DIR, "models", "roberta"),
+        max_epochs=4,
+    )
+    print(">> train roberta model done")
 
-print(">> predict eval roberta model")
-scores, est_acc = roberta_predict_eval(
-    data_labeled=join(_THIS_DIR, "demo_data", "valid_labeled.csv"),
-    data_unlabeled=join(_THIS_DIR, "demo_data", "valid_unlabeled.csv"),
-    model_dir=join(_THIS_DIR, "models", "roberta"),
-)
-print(">> predict eval roberta model done")
-print(">> estimated acc:", est_acc)
-print(">> scores shape:", scores.shape)
+    print(">> predict eval roberta model")
+    scores, est_acc = roberta_predict_eval(
+        data_labeled=join(_THIS_DIR, "demo_data", "valid_labeled.csv"),
+        data_unlabeled=join(_THIS_DIR, "demo_data", "valid_unlabeled.csv"),
+        model_dir=join(_THIS_DIR, "models", "roberta"),
+    )
+    print(">> predict eval roberta model done")
+    print(">> estimated acc:", est_acc)
+    print(">> scores shape:", scores.shape)
