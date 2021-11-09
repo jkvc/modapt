fece05689f8c9b0634f49b51ded7a3bbee1ab5fe
Junshen Kevin Chen <kevinehc@gmail.com>
2021-11-08 23:06:02 -0800

    upload to pypi v01

---
+++ modapt/__init__.py
@@ -0,0 +1,6 @@
+from modapt.modapt import (
+    logreg_predict_eval,
+    logreg_train,
+    roberta_predict_eval,
+    roberta_train,
+)
---
+++ setup.py
@@ -0,0 +1,30 @@
+from distutils.core import setup
+
+setup(
+    name="modapt",
+    packages=["modapt"],
+    version="0.1",  # update for new ver
+    license="MIT",
+    description="Modular Domain Adaptation, https://github.com/jkvc/modapt",
+    author="jkvc",
+    author_email="kevinehc@gmail.com",
+    url="https://github.com/jkvc/modapt",
+    download_url="https://github.com/jkvc/modapt/archive/refs/tags/v_01.tar.gz",  # update for new ver
+    keywords=["modapt", "modular", "domain", "adaptation"],
+    install_requires=[
+        "pandas",
+        "torchvision",
+        "nltk",
+        "matplotlib",
+        "transformers",
+        "sklearn",
+        "tensorboard",
+    ],
+    classifiers=[
+        "Development Status :: 4 - Beta",  # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
+        "Intended Audience :: Developers",  # Define that your audience are developers
+        "Topic :: Software Development :: Build Tools",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3.7",
+    ],
+)
